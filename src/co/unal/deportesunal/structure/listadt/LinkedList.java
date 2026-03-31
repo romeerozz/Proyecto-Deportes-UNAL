6a5405ef9dbe659801e41c7d571992090965f8da
Torito9609 <toroandres85@gmail.com>
2026-03-31 15:02:16 -0500

    creacion de contraro para linked list, inicio de implementacion para student

--- src/co/unal/deportesunal/structure/listadt/LinkedList.java
+++ src/co/unal/deportesunal/structure/listadt/LinkedList.java
@@ -1,4 +1,63 @@
 package co.unal.deportesunal.structure.listadt;
 
-public class LinkedList<T> {
+public class LinkedList<T> implements listAdt<T>{
+    @Override
+    public void pushFront(T value) {
+
+    }
+
+    @Override
+    public void pushBack(T value) {
+
+    }
+
+    @Override
+    public T popFront() {
+        return null;
+    }
+
+    @Override
+    public T popBack() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public T topFront() {
+        return null;
+    }
+
+    @Override
+    public T topBack() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Position<T> find(T value) {
+        return null;
+    }
+
+    @Override
+    public void erase(Position<T> position) {
+
+    }
+
+    @Override
+    public void addBefore(Position<T> position, T value) {
+
+    }
+
+    @Override
+    public void addAfter(Position<T> position, T value) {
+
+    }
 }
