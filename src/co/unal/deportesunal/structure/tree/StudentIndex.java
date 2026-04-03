bf86eaf1e351924d7b05f636850130bed74af6b8
Torito9609 <toroandres85@gmail.com>
2026-04-03 14:54:05 -0500

    javadoc agregado a la interfaz studentindex para generar claridad

--- src/co/unal/deportesunal/structure/tree/StudentIndex.java
+++ src/co/unal/deportesunal/structure/tree/StudentIndex.java
@@ -6,10 +6,32 @@ import co.unal.deportesunal.domain.exception.NotFoundException;
 import co.unal.deportesunal.structure.listadt.LinkedList;
 
 public interface StudentIndex {
-	void put(int id, Student student) throws DuplicatedIdException;
-	Student get(int id) throws NotFoundException;
-	boolean remove(int id);
-	LinkedList<Student> valuesInOrder();
-	boolean contains(int id);
-	int size();
+
+    /**
+     * Inserta un estudiante indexado por su ID.
+     * @throws DuplicatedIdException si el ID ya existe.
+     */
+    void put(int id, Student student) throws DuplicatedIdException;
+
+    /**
+     * Retorna el estudiante asociado a un ID.
+     * @throws NotFoundException si el ID no existe.
+     */
+    Student get(int id) throws NotFoundException;
+
+    /**
+     * Elimina el estudiante por ID.
+     * @return true si eliminó, false si el ID no existía.
+     */
+    boolean remove(int id);
+
+    /**
+     * Retorna todos los estudiantes (AVL/BST: en orden ascendente por ID).
+     * ListStudentIndex: en orden de inserción.
+     */
+    LinkedList<Student> valuesInOrder();
+
+    boolean contains(int id);
+
+    int size();
 }
