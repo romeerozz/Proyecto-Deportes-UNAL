6bcfcba8862142fdf27aaf42b03378b4b3d5b3f5
Torito9609 <toroandres85@gmail.com>
2026-04-03 15:22:48 -0500

    ListIndex implementado para hacer pruebas y benchmarking usando LinkedList como EDD principal, StudentService implementado

--- src/co/unal/deportesunal/service/StudentService.java
+++ src/co/unal/deportesunal/service/StudentService.java
@@ -1,37 +1,62 @@
 package co.unal.deportesunal.service;
 
 import co.unal.deportesunal.domain.Student;
+import co.unal.deportesunal.domain.SportEnum;
+import co.unal.deportesunal.domain.exception.DuplicatedIdException;
+import co.unal.deportesunal.domain.exception.NotFoundException;
 import co.unal.deportesunal.structure.listadt.LinkedList;
 import co.unal.deportesunal.structure.tree.StudentIndex;
 
 public class StudentService {
-	private final StudentIndex studentIndex;
 
-	public StudentService(StudentIndex studentIndex) {
-		this.studentIndex = studentIndex;
-	}
+    private final StudentIndex index;
 
-	public void registerStudent(Student student) {
-		studentIndex.put(student.getID(), student);
-	}
+    public StudentService(StudentIndex index) {
+        this.index = index;
+    }
 
-	public Student findStudentById(int id) {
-		return studentIndex.get(id);
-	}
+    public void registerStudent(Student student) throws DuplicatedIdException {
+        if (student == null) throw new IllegalArgumentException("Student cannot be null.");
+        index.put(student.getId(), student);
+    }
 
-	public boolean removeStudentById(int id) {
-		return studentIndex.remove(id);
-	}
+    public Student findStudentById(int id) throws NotFoundException {
+        return index.get(id);
+    }
 
-	public boolean exists(int id) {
-		return studentIndex.contains(id);
-	}
+    public boolean removeStudentById(int id) {
+        return index.remove(id);
+    }
 
-	public LinkedList<Student> listStudentsOrderedById() {
-		return studentIndex.valuesInOrder();
-	}
+    public boolean exists(int id) {
+        return index.contains(id);
+    }
 
-	public int totalStudents() {
-		return studentIndex.size();
-	}
+    public int totalStudents() {
+        return index.size();
+    }
+
+    public LinkedList<Student> listStudentsOrderedById() {
+        return index.valuesInOrder();
+    }
+
+    public boolean addPracticeSport(int studentId, SportEnum sport) throws NotFoundException {
+        Student s = index.get(studentId);
+        return s.addPractice(sport);
+    }
+
+    public boolean addInterestSport(int studentId, SportEnum sport) throws NotFoundException {
+        Student s = index.get(studentId);
+        return s.addInterest(sport);
+    }
+
+    public boolean removePracticeSport(int studentId, SportEnum sport) throws NotFoundException {
+        Student s = index.get(studentId);
+        return s.removePractice(sport);
+    }
+
+    public boolean removeInterestSport(int studentId, SportEnum sport) throws NotFoundException {
+        Student s = index.get(studentId);
+        return s.removeInterest(sport);
+    }
 }
