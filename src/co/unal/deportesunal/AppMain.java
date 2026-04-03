9055aef0feb92d297be2da29c3dd6ffd19fa2f5f
Torito9609 <toroandres85@gmail.com>
2026-04-03 18:09:45 -0500

    AppMain implementado, CRUD interactivo por consola funcionando con persistencia

--- src/co/unal/deportesunal/AppMain.java
+++ src/co/unal/deportesunal/AppMain.java
@@ -1,8 +1,20 @@
 package co.unal.deportesunal;
 
+import co.unal.deportesunal.controller.AppController;
+import co.unal.deportesunal.persistence.StudentRepository;
+import co.unal.deportesunal.persistence.TxtStudentRepository;
+import co.unal.deportesunal.service.StudentService;
+import co.unal.deportesunal.structure.tree.ListStudentIndex;
+import co.unal.deportesunal.structure.tree.StudentIndex;
+import co.unal.deportesunal.ui.ConsoleUi;
+
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("commit de prueba rama kevin");
+        StudentIndex index = new ListStudentIndex();
+        StudentRepository repo = new TxtStudentRepository();
+        StudentService studentService= new StudentService(index, repo);
+        AppController controller = new AppController(studentService);
+        ConsoleUi ui = new ConsoleUi(controller);
+        ui.run();
     }
 }
