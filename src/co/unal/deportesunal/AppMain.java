1b6aacc67dc4f7be13d25e1e49660c8657f6e458
Torito9609 <toroandres85@gmail.com>
2026-05-01 14:53:44 -0500

    se hicieron pruebas para BstIndex y AvlIndex con el CRUD interactivo, funcionando

--- src/co/unal/deportesunal/AppMain.java
+++ src/co/unal/deportesunal/AppMain.java
@@ -4,13 +4,17 @@ import co.unal.deportesunal.controller.AppController;
 import co.unal.deportesunal.persistence.StudentRepository;
 import co.unal.deportesunal.persistence.TxtStudentRepository;
 import co.unal.deportesunal.service.StudentService;
+import co.unal.deportesunal.structure.index.AvlIndex;
+import co.unal.deportesunal.structure.index.BstIndex;
 import co.unal.deportesunal.structure.index.ListStudentIndex;
 import co.unal.deportesunal.structure.index.StudentIndex;
 import co.unal.deportesunal.ui.ConsoleUi;
 
 public class AppMain {
     public static void main(String[] args) {
-        StudentIndex index = new ListStudentIndex();
+        StudentIndex index = new BstIndex();
+        //StudentIndex index = new AvlIndex();
+        //StudentIndex index = new ListStudentIndex();
         StudentRepository repo = new TxtStudentRepository();
         StudentService studentService= new StudentService(index, repo);
         AppController controller = new AppController(studentService);
