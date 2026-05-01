0a325a71f0b67b7b427da81f73f92e067f24e1a7
Torito9609 <toroandres85@gmail.com>
2026-05-01 15:11:40 -0500

    se modifica la persistencia para poder utilizar diferentes txt, para students del crud interactivo y para el mock data de diferentes tamaños

--- src/co/unal/deportesunal/persistence/FileConstant.java
+++ src/co/unal/deportesunal/persistence/FileConstant.java
@@ -3,7 +3,25 @@ package co.unal.deportesunal.persistence;
 import java.io.File;
 
 public class FileConstant {
+    public static final String BASE_DIR =
+            System.getProperty("user.dir") + File.separator + "data";
+
+    public static final String PERSISTENCE_DIR =
+            BASE_DIR + File.separator + "persistence";
+
+    public static final String MOCK_DIR =
+            BASE_DIR + File.separator + "mock";
+
+    public static final String RESULTS_DIR =
+            BASE_DIR + File.separator + "results";
+
     public static final String STUDENTS_FILE =
-            System.getProperty("user.dir") + File.separator +
-                    "data" + File.separator + "persistence" + File.separator + "students.txt";
+            PERSISTENCE_DIR + File.separator + "students.txt";
+
+    public static String mockStudentsFile(int n, long seed) {
+        return MOCK_DIR + File.separator + "students_" + n + "_seed" + seed + ".txt";
+    }
+
+    public static final String INDEX_BENCHMARK_RESULTS =
+            RESULTS_DIR + File.separator + "index_benchmark.csv";
 }
