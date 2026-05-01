d7a24f4944d50a72a61da327ce3a96e7879862ef
Torito9609 <toroandres85@gmail.com>
2026-05-01 15:47:22 -0500

    se implementa el mock data generator, se diferencia entre unique ids o ids con repeticion para diferenciar en los queries de delete o search y evitar oeraciones no exitosas que contaminen los benchmark

--- src/co/unal/deportesunal/benchmark/TreeBenchmark.java
+++
@@ -1,8 +0,0 @@
-package co.unal.deportesunal.benchmark;
-
-
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class TreeBenchmark {
-}
---
+++ src/co/unal/deportesunal/benchmark/utils/SimpleCsvWriter.java
@@ -0,0 +1,20 @@
+package co.unal.deportesunal.benchmark.utils;
+
+import java.io.IOException;
+
+public class SimpleCsvWriter implements CsvWriter{
+    @Override
+    public void writeHeader(String... cols) {
+
+    }
+
+    @Override
+    public void writeRow(String... values) {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
